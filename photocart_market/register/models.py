bb03a05af24e7898a296963010eb5c332d8e7da9
sjinh88 <sjinh88@gmail.com>
2024-07-17 03:30:41 +0900

    modeling...

---
+++ photocart_market/register/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class PhotoCard(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    photo_card = models.ImageField(blank=True, upload_to="photo_card/")
+    ea = models.PositiveSmallIntegerField(default=1)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
